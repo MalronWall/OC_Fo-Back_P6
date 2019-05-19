161258f38521295a8fd2fea09709d2360c0e0e73
MalronWall <thibaut.tourte17@gmail.com>
2019-05-19 19:34:15 +0200

    [TEST] First test for sending email on a prod server

--- src/Application/Helpers/MailerHelper.php
+++ src/Application/Helpers/MailerHelper.php
@@ -44,10 +44,7 @@ class MailerHelper implements MailerHelperInterface
      */
     public function sendEmail($subject, $from, $to, $user)
     {
-        /** @var \Swift_Message $message */
-        $message = $this->mailer->createMessage();
-        $message
-            ->setSubject($subject)
+        $message = (new \Swift_Message($subject))
             ->setFrom($from)
             ->setTo($to)
             ->setBody(
